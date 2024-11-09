601bf9a82be0d61d91cbd7669bb4bc8938fc9cd5
Dan Erwin Magno <danerwin111@gmail.com>
2024-11-10 00:01:30 +0800

    Update MainView.vue

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -19,36 +19,34 @@ const onWheel = event => {
 const wheelup = () => scrollNavigation(-1)
 const wheeldown = () => scrollNavigation(1)
 
-const scrollNavigation = async direction => {
-  animateOnscroll()
+const scrollNavigation = direction => {
+  distanceToMove.value += direction > 0 ? -200 : 200
+  animateOnscroll(direction)
 
-  const removedNav =
-    direction > 0 ? navItems.value.shift() : navItems.value.pop()
-  direction > 0
-    ? navItems.value.push(removedNav)
-    : navItems.value.unshift(removedNav)
-
-  await nextTick()
-  const listItems = document.querySelectorAll('.nav-item-container')
-  animate(
-    listItems[direction > 0 ? 0 : listItems.length - 1],
-    {
-      height: '200px',
-    },
-    { duration: 0 },
-  )
+  // const removedNav =
+  //   direction > 0 ? navItems.value.shift() : navItems.value.pop()
+  // direction > 0
+  //   ? navItems.value.push(removedNav)
+  //   : navItems.value.unshift(removedNav)
 }
 
-const animateOnscroll = async () => {
+const distanceToMove = ref(0)
+
+watch(distanceToMove, newVal => {
+  console.log(newVal)
+})
+
+const animateOnscroll = async direction => {
   await nextTick()
-  const listItems = document.querySelectorAll('.nav-item')
+  const listItems = document.querySelectorAll('.nav-item-container')
   listItems.forEach((element, index) => {
-    animate(element, { scale: index === 2 ? 1 : 0.7 }, { duration: 1 })
+    animate(element, { y: distanceToMove.value }, { duration: 1 })
   })
 }
 
 onMounted(() => {
-  animateOnscroll()
+  const listItems = document.querySelectorAll('.nav-item-container')
+
   addEventListener('wheel', onWheel)
 })
 
@@ -105,15 +103,14 @@ onUnmounted(() => {
   flex-direction: column;
   justify-content: space-between;
   position: absolute;
-  transform: translateY(-33%);
   height: 600px;
-  background-color: red;
 }
 
 .nav-item-container {
   display: flex;
   align-items: center;
   height: 200px;
+  border: 1px solid green;
 }
 
 .nav-item {
