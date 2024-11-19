b08e7235b93c795d1ce23c847b300618b6e7fe80
Dan Erwin Magno <danerwin111@gmail.com>
2024-11-19 23:53:22 +0800

    Update AboutSection.vue

--- src/views/Pages/AboutSection.vue
+++ src/views/Pages/AboutSection.vue
@@ -44,37 +44,66 @@ onMounted(() => {
 })
 
 const isHovering = ref(false)
+
+const floatingInfoData = [
+    {
+        id: 1,
+        title: 'Personal Info',
+        contents: ['test', 'test'],
+        position: 'top-left',
+    },
+    {
+        id: 2,
+        title: 'Personal Info',
+        contents: ['test', 'test'],
+        position: 'top-right',
+    },
+    {
+        id: 3,
+        title: 'Personal Info',
+        contents: ['test', 'test'],
+        position: 'bottom-left',
+    },
+    {
+        id: 4,
+        title: 'Personal Info',
+        contents: ['test', 'test'],
+        position: 'bottom-right',
+    },
+]
 </script>
 
 <template>
-    <section>
+    <section class="about-section">
         <DynamicHeading title="ABOUT SECTION" subtitle="1.0" />
-        <div class="profile">
-            <div class="sub-heading sub-heading-1">
-                1.1 It's Getting Personal
-            </div>
+        <div class="sub-section">
+            <div class="sub-heading">1.1 It's Getting Personal</div>
             <div class="img-container">
                 <DoodleFace />
                 <FloatingInfo
-                    title="Personal Info"
-                    :contents="['Test', 'test']"
-                    position="top-left"
+                    v-for="data in floatingInfoData"
+                    :key="data.id"
+                    :title="data.title"
+                    :contents="data.contents"
+                    :position="data.position"
                 />
-                <FloatingInfo
-                    title="Personal Info"
-                    :contents="['Test', 'test']"
-                    position="top-right"
-                />
-                <FloatingInfo
-                    title="Personal Info"
-                    :contents="['Test', 'test']"
-                    position="bottom-left"
-                />
-                <FloatingInfo
-                    title="Personal Info"
-                    :contents="['Test', 'test']"
-                    position="bottom-right"
-                />
+            </div>
+        </div>
+
+        <div class="sub-section">
+            <div class="sub-heading">1.2 Life story, nothing special</div>
+            <div class="exp-container">
+                <div class="timeline-text">Timeline</div>
+                <div class="line"></div>
+                <div class="timeline-content">
+                    <div v-for="i in 5" :key="i">
+                        <div class="line-content">
+                            <div class="circle"></div>
+                            <div class="date">Date Here Kunuhay</div>
+                        </div>
+                        <div class="content"></div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
@@ -84,13 +113,15 @@ const isHovering = ref(false)
 section {
     display: flex;
     flex-direction: column;
-    gap: 1rem;
+    gap: 12rem;
 }
 
-.profile {
-    width: 100%;
-    margin: 0 auto;
+.about-section > .sub-section:nth-child(2) {
     margin-top: 50rem;
+}
+
+.sub-section {
+    width: 100%;
 }
 
 .sub-heading {
@@ -108,5 +139,58 @@ section {
     display: flex;
     justify-content: center;
     align-items: center;
+}
+
+.exp-container {
+    display: flex;
+}
+
+.timeline-text {
+    width: 10%;
+}
+
+.line {
+    height: 1500px;
+    width: 5px;
+    border-radius: 5px;
+    background-color: red;
+}
+
+.timeline-content {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    flex: 1;
+}
+
+.timeline-content > div {
+    flex: 1 1 0;
+    display: flex;
+    align-items: center;
+}
+
+.line-content {
+    width: 20%;
+    display: flex;
+    align-items: center;
+}
+
+.circle {
+    width: 10px;
+    height: 10px;
+    background-color: yellow;
+    border-radius: 100%;
+    position: relative;
+    left: -8px;
+}
+
+.date {
+    margin: 0 auto;
+}
+
+.content {
+    flex: 1 1 0;
+    height: 100%;
+    border: 1px solid white;
 }
 </style>
