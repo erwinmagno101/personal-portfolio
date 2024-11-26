8053559049f84bbe07d4d6adc013d18ee87ddab7
Dan Erwin Magno <danerwin111@gmail.com>
2024-11-26 17:09:05 +0800

    Create SectionComponent.vue

---
+++ src/components/SectionComponent.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+    <section>
+        <slot></slot>
+    </section>
+</template>
+
+<style scoped>
+section {
+    display: flex;
+    flex-direction: column;
+    min-height: 90vh;
+}
+</style>
