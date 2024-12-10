d3b6ed204dcaa664755ec3d06bbb706f0b4e7950
Dan Erwin Magno <danerwin111@gmail.com>
2024-12-10 17:08:06 +0800

    Update MainView.vue

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,5 +1,7 @@
 <script setup>
 import HeroSection from '@/sections/HeroSection.vue'
+import { animate } from 'motion'
+import { onMounted, onUnmounted, ref } from 'vue'
 
 const navList = [
     { title: 'Home' },
@@ -8,10 +10,50 @@ const navList = [
     { title: 'About' },
     { title: 'Contact' },
 ]
+const cameraRef = ref(null)
+let isAnimating = false
+
+const handleMouseTracking = e => {
+    if (isAnimating) return
+
+    isAnimating = true
+
+    requestAnimationFrame(() => {
+        const cameraRect = cameraRef.value.getBoundingClientRect()
+
+        let yDistanceToMove = e.clientY - cameraRect.height / 2
+        let xDistanceToMove = e.clientX - cameraRect.width / 2
+
+        animate(
+            cameraRef.value,
+            {
+                top: `${yDistanceToMove}px`,
+                left: `${xDistanceToMove}px`,
+            },
+            { duration: 0.3 },
+        )
+
+        isAnimating = false
+    })
+}
+
+onMounted(() => {
+    document.addEventListener('mousemove', handleMouseTracking)
+})
+
+onUnmounted(() => {
+    document.removeEventListener('mousemove', handleMouseTracking)
+})
 </script>
 
 <template>
-    <div>
+    <div class="canvas">
+        <div class="background-grid">
+            <div class="camera" ref="cameraRef"></div>
+            <div class="grid-box-container" v-for="i in 50" :key="i">
+                <div class="grid-box"></div>
+            </div>
+        </div>
         <header>
             <div class="Logo">LOGO HERE</div>
             <nav>
@@ -29,8 +71,38 @@ const navList = [
 </template>
 
 <style scoped>
-div {
+.canvas {
     font-family: 'Google Sans Flex', sans-serif;
+    position: relative;
+    overflow-x: hidden;
+}
+
+.background-grid {
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    z-index: -99;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, calc(100vw / 10));
+    grid-template-rows: repeat(auto-fill, minmax(calc(100vh / 5)));
+}
+
+.grid-box-container {
+    padding: 5px;
+}
+
+.camera {
+    width: 300px;
+    height: 300px;
+    background-color: white;
+    position: absolute;
+    z-index: -98;
+}
+
+.grid-box {
+    width: 100%;
+    height: 100%;
+    background-color: var(--primary-color);
 }
 
 header {
