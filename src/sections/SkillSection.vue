3f1df651958240c4dcb29ce4f3ebd2554f7180fd
Dan Erwin Magno <danerwin111@gmail.com>
2024-12-13 13:27:16 +0800

    ExperienceDate Component

--- src/sections/SkillSection.vue
+++ src/sections/SkillSection.vue
@@ -1,49 +1,7 @@
 <script setup>
+import ExperienceDate from '@/components/ExperienceDate.vue'
 import SectionHeader from '@/components/SectionHeader.vue'
 import TechGrid from '@/components/TechGrid.vue'
-import { onMounted, ref, onUnmounted, computed } from 'vue'
-
-const pastPracticeDate = new Date('2017-06-01T00:00:00')
-const pastProfessionalDate = new Date('2023-06-01T00:00:00')
-const pastBirthDate = new Date('2000-10-25T00:00:00')
-
-const now = ref(new Date())
-
-function calculateTimeDifference(past, current) {
-    const diffInSeconds = Math.floor((current - past) / 1000)
-    const seconds = diffInSeconds % 60
-    const minutes = Math.floor(diffInSeconds / 60) % 60
-    const hours = Math.floor(diffInSeconds / 3600) % 24
-    const days = Math.floor(diffInSeconds / 86400) % 30
-    const months = Math.floor(diffInSeconds / 2592000) % 12
-    const years = Math.floor(diffInSeconds / 31536000)
-
-    return { years, months, days, hours, minutes, seconds }
-}
-
-const formattedPractice = computed(() => {
-    return calculateTimeDifference(pastPracticeDate, now.value)
-})
-
-const formattedProfessional = computed(() => {
-    return calculateTimeDifference(pastProfessionalDate, now.value)
-})
-
-const formattedBirth = computed(() => {
-    return calculateTimeDifference(pastBirthDate, now.value)
-})
-
-let intervalId
-onMounted(() => {
-    intervalId = setInterval(() => {
-        now.value = new Date()
-    }, 1000)
-})
-
-// Clear interval on unmount
-onUnmounted(() => {
-    clearInterval(intervalId)
-})
 </script>
 
 <template>
@@ -62,31 +20,7 @@ onUnmounted(() => {
                 <p class="sub-description">
                     Honed throughout my coding journey
                 </p>
-                <div class="experience-container">
-                    <div class="date">
-                        <div>
-                            {{ formattedPractice.years }}
-                            <div>years</div>
-                            <div>coding practice</div>
-                        </div>
-                        <div>
-                            {{ formattedProfessional.years }}
-                            <div>
-                                {{
-                                    formattedProfessional.years > 1
-                                        ? 'years'
-                                        : 'year'
-                                }}
-                            </div>
-                            <div>professional experience</div>
-                        </div>
-                        <div>
-                            {{ formattedBirth.years }}
-                            <div>years</div>
-                            <div>being alive</div>
-                        </div>
-                    </div>
-                </div>
+                <ExperienceDate />
             </div>
         </div>
     </section>
@@ -115,33 +49,5 @@ onUnmounted(() => {
 .sub-description {
     text-align: center;
     margin-bottom: 50px;
-}
-
-.experience-container {
-    display: flex;
-    width: fit-content;
-    margin: 0 auto;
-    gap: 5rem;
-    font-size: 4rem;
-    font-weight: 700;
-}
-
-.date {
-    display: flex;
-    gap: 5rem;
-}
-
-.date > div {
-    display: flex;
-    font-size: 6rem;
-    flex-direction: column;
-    line-height: 1;
-    align-items: center;
-    font-weight: 600;
-}
-
-.date > div > div {
-    font-size: 1rem;
-    font-weight: 400;
 }
 </style>
