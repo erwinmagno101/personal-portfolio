27475fade44c36fe1e12c4459f38c531d1795b0c
Dan Erwin Magno <danerwin111@gmail.com>
2024-12-17 17:25:31 +0800

    Update BackgroundEffect.vue

--- src/components/BackgroundEffect.vue
+++ src/components/BackgroundEffect.vue
@@ -41,7 +41,7 @@ onUnmounted(() => {
 <template>
     <div class="background-grid">
         <div class="camera" ref="cameraRef"></div>
-        <div class="grid-box-container" v-for="i in 50" :key="i">
+        <div class="grid-box-container" v-for="i in 190" :key="i">
             <div class="grid-box"></div>
         </div>
     </div>
@@ -54,25 +54,26 @@ onUnmounted(() => {
     position: fixed;
     z-index: -99;
     display: grid;
-    grid-template-columns: repeat(auto-fill, calc(100vw / 10));
-    grid-template-rows: repeat(auto-fill, minmax(calc(100vh / 5)));
+    grid-template-columns: repeat(auto-fill, calc(100vw / 19));
+    opacity: 0.08;
+    filter: blur(1px);
 }
 
 .grid-box-container {
-    padding: 5px;
+    padding: 1px;
 }
 
 .camera {
-    width: 300px;
-    height: 300px;
-    background-color: white;
+    width: 150px;
+    height: 150px;
+    background-color: black;
     position: absolute;
     z-index: -98;
 }
 
 .grid-box {
     width: 100%;
-    height: 100%;
     background-color: var(--primary-color);
+    aspect-ratio: 1;
 }
 </style>
