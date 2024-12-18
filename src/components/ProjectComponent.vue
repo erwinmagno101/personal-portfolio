35ac67f07457ec9357cc76c6155c0dcd9935d69b
Dan Erwin Magno <danerwin111@gmail.com>
2024-12-18 23:30:30 +0800

    Create ProjectComponent.vue

---
+++ src/components/ProjectComponent.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+
+<template>
+    <div class="projects-container">
+        <div class="project-block" v-for="i in 3" :key="i">gg</div>
+    </div>
+</template>
+
+<style scoped>
+.projects-container {
+    width: 80%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    align-items: center;
+    padding: 1rem;
+}
+
+.project-block {
+    width: 100%;
+    border: 1px solid black;
+}
+</style>
