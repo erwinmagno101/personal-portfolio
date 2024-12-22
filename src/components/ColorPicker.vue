051fb20026ee0dbaae40520cf1191cdac2f80b7f
Dan Erwin Magno <danerwin111@gmail.com>
2024-12-22 18:29:42 +0800

    Color Gen

---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { onMounted, ref, watch } from 'vue'
+import { useColorGeneration } from '@/stores/colorgen'
+
+const colorGeneration = useColorGeneration()
+
+const currentColor = ref(null)
+
+const generateColor = () => {
+    currentColor.value = colorGeneration.generateRandomColor()
+
+    document.documentElement.style.setProperty(
+        '--primary-color',
+        currentColor.value.colors.primary,
+    )
+
+    document.documentElement.style.setProperty(
+        '--accent-color',
+        currentColor.value.colors.secondary,
+    )
+}
+</script>
+
+<template>
+    <div @click="generateColor">generate</div>
+</template>
