88b07f5c5ad8da637e2581365a995681dad1c920
Dan Erwin Magno <danerwin111@gmail.com>
2024-12-26 17:08:50 +0800

    pckg

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script setup>
 import MainView from './views/MainView.vue'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+    document.title = 'Developer Portfolio' // Change the browser tab title
+})
 </script>
 
 <template>
