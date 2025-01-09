70fa26570ec6b7c8ad5582c5bf94b0e4a43923da
Dan Erwin Magno <danerwin111@gmail.com>
2025-01-09 08:59:13 +0800

    Create NavBar.vue

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+    <nav>
+        <div>LOGO</div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+        <div>
+            <div>Get in touch</div>
+        </div>
+    </nav>
+</template>
