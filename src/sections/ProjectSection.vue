b93fd96027ac054fb629f66868efe6729fee50aa
Dan Erwin Magno <danerwin111@gmail.com>
2025-01-15 13:57:37 +0800

    Create ProjectSection.vue

---
+++ src/sections/ProjectSection.vue
@@ -0,0 +1,33 @@
+<script setup></script>
+<template>
+    <section>
+        <div>
+            <h2>Featured Projects</h2>
+            <div class="projects-container">
+                <div class="project" v-for="i in 3" :key="i"></div>
+            </div>
+        </div>
+
+        <div>
+            <h2>Personal Projects</h2>
+            <div class="projects-container"></div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+.projects-container {
+    display: flex;
+    gap: 1rem;
+}
+
+section div > :first-child {
+    margin-bottom: 1rem;
+}
+
+.project {
+    flex: 1;
+    background: red;
+    height: 350px;
+}
+</style>
